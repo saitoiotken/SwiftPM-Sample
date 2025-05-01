f66234a18ce0f46dc3cf020dce8d8dcc57b38f11
saitoiotken <saitoiotken@users.noreply.github.com>
2025-05-01 14:21:41 +0900

    Fix

--- Tests/FluctSDKPackageTests/FluctSDKPackageTests.swift
+++ Tests/FluctSDKPackageTests/FluctSDKPackageTests.swift
@@ -6,5 +6,6 @@ final class FluctSDKTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
+        print(FluctSDK.version())
     }
 }
