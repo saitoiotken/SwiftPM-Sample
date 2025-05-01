32538213ef26b7de5cee59ae03e3bdcf54999e2d
saitoiotken <saitoiotken@users.noreply.github.com>
2025-05-01 14:16:41 +0900

    Fix

--- Tests/FluctSDKPackageTests/FluctSDKPackageTests.swift
+++ Tests/FluctSDKPackageTests/FluctSDKPackageTests.swift
@@ -1,25 +1,10 @@
 import XCTest
-@testable import FluctSDKPackage
+@testable import FluctSDK
 
-final class FluctSDKPackageTests: XCTestCase {
+final class FluctSDKTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(FluctSDKPackage().text, "Hello, World!")
-    }
-    func testBundle() throws {
-        if let bundleURL = Bundle.module.url(forResource: "FluctSDKResources", withExtension: "bundle") {
-            if let resourceBundle = Bundle(url: bundleURL) {
-                // ここでresourceBundleを使ってリソースにアクセスできます
-                if let imageURL = UIImage(named: "btn_rewarded_video_voice_on", in: resourceBundle, compatibleWith: nil) {
-                    // リソースのURLが取得できました。画像などをロードできます。
-                    print("Image URL: \(imageURL)")
-                }
-                if let js = resourceBundle.url(forResource: "mraid", withExtension: "js") {
-                    print("js: \(js)")
-                }
-            }
-        }
     }
 }
